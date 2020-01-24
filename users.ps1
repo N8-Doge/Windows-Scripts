42303ba0e5289f0d372dd7f3931e41e3dc8dad3b
N8-Doge <36688360+N8-Doge@users.noreply.github.com>
2020-01-24 12:06:29 -0800

    Update users.ps1

--- users.ps1
+++ users.ps1
@@ -20,8 +20,24 @@ if(!(test-path $Desktop\Users.txt)){
 		./parse.ps1
 	}
 }
+#----------[ Functions ]----------
+# Check password security
+function check-password($pw){
+    if($pw -cmatch "[a-z]"){$i++}
+    if($pw -cmatch "[A-Z]"){$i++}
+    if($pw -cmatch "[0-9]"){$i++}
+    if($pw -cmatch "[^a-zA-Z0-9]"){$i++}
+    return ($i -ge 3)
+}
 
 #----------[ Main Execution ]----------
+# Create a password string for all users
+$plaintxt = ""
+while(-not (check-password $plaintxt)){
+    $plaintxt = read-host "Please enter a secure password"
+}
+$encrypt = convertto-securestring -asplain $plaintxt -force
+
 # Executes the appropriate user script
 gcm get-aduser 2>&1> $null
 if($?){
