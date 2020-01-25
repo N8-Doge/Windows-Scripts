5589d3b567fab0ca1a91e759f05b47225adcc8f1
N8-Doge <nathantchennnttcc@gmail.com>
2020-01-25 01:45:47 -0800

    Finished files.ps1

---
+++ files.ps1
@@ -0,0 +1,14 @@
+﻿$searchPath = "C:\"
+$fileTypes = (ls $searchPath -Recurse).Extension
+$fileTypes = $fileTypes | Select-Object -Unique | Where-Object {$_}
+$log = "$home\Desktop\files.txt"
+$null > $log
+
+
+forEach($ext in $fileTypes){
+    "Searching for: $ext's"
+    "*****" >> $log
+    "$ext " >> $log
+    "*****" >> $log
+    ls $searchPath -Recurse -Filter *$ext | Select-Object -ExpandProperty FullName >> $log
+}
